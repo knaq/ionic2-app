831966bcf100a0082cdafe4e7d73614223476377
Shrinivas Kopparam Ramanath <shrini@uw.edu>
2016-04-19 14:46:17 -0700

    Added tutorial carousel that pushes start page

--- app/pages/tabs/tabs.js
+++ app/pages/tabs/tabs.js
@@ -2,7 +2,7 @@ import {Page} from 'ionic-angular';
 import {Page1} from '../page1/page1';
 import {Profile} from '../profile/profile';
 import {Page3} from '../page3/page3';
-import {Tutorial} from '../tutorial/tutorial';
+import {TutorialPage} from '../tutorial/tutorial';
 import {PortfolioDetailPage} from '../portfolio-detail/portfolio-detail'
 import {SkillDetailPage} from '../skill-detail/skill-detail'
 
@@ -16,7 +16,7 @@ export class TabsPage {
     this.tab1Root = Page1;
     this.tab2Root = Profile;
     this.tab3Root = Page3;
-    this.tab4Root = Tutorial;
+    this.tab4Root = TutorialPage;
     this.tab5Root = PortfolioDetailPage;
     this.tab6Root = SkillDetailPage;
   }
