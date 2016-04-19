15e713aed82dd52cd2ecab3bf586c888bb76ff77
Shrinivas Kopparam Ramanath <shrini@uw.edu>
2016-04-19 13:23:14 -0700

    rename to profile page

--- app/pages/profile/profile.js
+++ app/pages/profile/profile.js
@@ -36,9 +36,9 @@ import {Page} from 'ionic-angular';
 
 
 @Page({
-  templateUrl: 'build/pages/page2/page2.html'
+  templateUrl: 'build/pages/profile/profile.html'
 })
-export class Page2 {
+export class Profile {
   constructor() {
 
   }
